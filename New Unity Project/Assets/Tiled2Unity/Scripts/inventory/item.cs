ee2137afc4d314cec40d958dea45da07f7d84854
threepat1 <42788704+threepat1@users.noreply.github.com>
2019-08-22 22:30:40 +1000

    make script and data

---
+++ New Unity Project/Assets/Tiled2Unity/Scripts/inventory/item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : ScriptableObject
+{
+    public string tk_itemName;
+    public Sprite tk_icon;
+
+    public virtual void tk_Use()
+    {
+
+    }
+
+}
